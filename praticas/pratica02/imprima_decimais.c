453119efad92e93ef58be46bef15f0a3024c90ed
unknown <2612082020@ACADEMICO.IESB.BR>
2026-04-15 10:57:38 -0300

    feat: conclui pratica02. Fecha #02

---
+++ praticas/pratica02/imprima_decimais.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main(){
+double duplicado;
+duplicado = 3.1415926;
+printf("%.7f\n", duplicado);
+
+double duplo;
+duplo = 3.1415926;
+printf("%.2f\n", duplo);
+
+
+
+    return 0;
+}
